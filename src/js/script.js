80c8626b405d1535480ef0d415bc955c8cd25c57
Oleg <osapogovskyj@gmail.com>
2022-09-30 21:38:07 +0300

    slider

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 8,
+        slidesToScroll: 1
+      });
+  });
